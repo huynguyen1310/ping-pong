294a8fb0ecb6ddf19a2db9e352396193b6c6f02c
huynguyen1310 <huynguyen1310@users.noreply.github.com>
2023-02-15 10:25:14 +0000

    PHP Linting (Pint)

--- app/Http/Controllers/API/PongController.php
+++ app/Http/Controllers/API/PongController.php
@@ -6,25 +6,24 @@ use App\Data\PongData;
 use App\Http\Controllers\Controller;
 use App\Models\Pong;
 use Illuminate\Support\Facades\Cache;
-use App\Models\Site;
 
 class PongController extends Controller
 {
-  public function index()
-  {
-    if (Cache::has('pongs')) {
-      return PongData::collection(Cache::get('pongs'));
+    public function index()
+    {
+        if (Cache::has('pongs')) {
+            return PongData::collection(Cache::get('pongs'));
+        }
+
+        $pongs = Cache::remember('pongs', 10, function () {
+            return Pong::take(10000)->get();
+        });
+
+        return PongData::collection($pongs);
     }
 
-    $pongs = Cache::remember('pongs', 10, function () {
-      return Pong::take(10000)->get();
-    });
-
-    return PongData::collection($pongs);
-  }
-
-  public function show(Pong $pong)
-  {
-    return PongData::from($pong);
-  }
+    public function show(Pong $pong)
+    {
+        return PongData::from($pong);
+    }
 }
