2c9490b6980d516ccd5fed35638d129277b77dcf
Nguyen Quoc Huy <>
2023-05-19 15:41:13 +0700

    no message

--- deploy.php
+++ deploy.php
@@ -14,9 +14,16 @@ add('writable_dirs', []);
 
 // Hosts
 
-host('146.190.93.45')
+host('dev')
+  ->setHostName('146.190.93.45')
   ->set('remote_user', 'web')
   ->set('deploy_path', '~/site');
+
+// Grabs the dotenv file from the github secret
+task('deploy:secrets', function () {
+  file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+  upload('.env', get('deploy_path') . '/shared');
+});
 
 // Hooks
 
