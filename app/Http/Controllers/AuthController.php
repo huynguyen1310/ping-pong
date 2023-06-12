7207263e329dcfb2aa30a2e378a11bbb8d41fed6
huynguyen1310 <huynguyen1310@users.noreply.github.com>
2023-06-12 08:56:21 +0000

    PHP Linting (Pint)

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,93 +10,93 @@ use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-  public function register(Request $request)
-  {
-    try {
-      //Validated
-      $validateUser = Validator::make(
-        $request->all(),
-        [
-          'name' => 'required',
-          'email' => 'required|email|unique:users,email',
-          'password' => 'required',
-        ]
-      );
+    public function register(Request $request)
+    {
+        try {
+            //Validated
+            $validateUser = Validator::make(
+                $request->all(),
+                [
+                    'name' => 'required',
+                    'email' => 'required|email|unique:users,email',
+                    'password' => 'required',
+                ]
+            );
 
-      if ($validateUser->fails()) {
-        return response()->json([
-          'status' => false,
-          'message' => 'validation error',
-          'errors' => $validateUser->errors(),
-        ], 401);
-      }
+            if ($validateUser->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors(),
+                ], 401);
+            }
 
-      $user = User::create([
-        'name' => $request->name,
-        'email' => $request->email,
-        'password' => Hash::make($request->password),
-      ]);
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
 
-      return response()->json([
-        'status' => true,
-        'message' => 'User Created Successfully',
-        'token' => $user->createToken('API TOKEN')->plainTextToken,
-      ], 200);
-    } catch (\Throwable $th) {
-      return response()->json([
-        'status' => false,
-        'message' => $th->getMessage(),
-      ], 500);
+            return response()->json([
+                'status' => true,
+                'message' => 'User Created Successfully',
+                'token' => $user->createToken('API TOKEN')->plainTextToken,
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
     }
-  }
 
-  public function login(Request $request)
-  {
-    try {
-      $validateUser = Validator::make(
-        $request->all(),
-        [
-          'email' => 'required|email',
-          'password' => 'required',
-        ]
-      );
+    public function login(Request $request)
+    {
+        try {
+            $validateUser = Validator::make(
+                $request->all(),
+                [
+                    'email' => 'required|email',
+                    'password' => 'required',
+                ]
+            );
 
-      if ($validateUser->fails()) {
-        return response()->json([
-          'status' => false,
-          'message' => 'validation error',
-          'errors' => $validateUser->errors(),
-        ], 401);
-      }
+            if ($validateUser->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors(),
+                ], 401);
+            }
 
-      if (!Auth::attempt($request->only(['email', 'password']))) {
-        return response()->json([
-          'status' => false,
-          'message' => 'Email & Password does not match with our record.',
-        ], 401);
-      }
+            if (! Auth::attempt($request->only(['email', 'password']))) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email & Password does not match with our record.',
+                ], 401);
+            }
 
-      $user = User::where('email', $request->email)->first();
+            $user = User::where('email', $request->email)->first();
 
-      return response()->json([
-        'status' => true,
-        'message' => 'User Logged In Successfully',
-        'token' => $user->createToken('API TOKEN')->plainTextToken,
-      ], 200);
-    } catch (\Throwable $th) {
-      return response()->json([
-        'status' => false,
-        'message' => $th->getMessage(),
-      ], 500);
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged In Successfully',
+                'token' => $user->createToken('API TOKEN')->plainTextToken,
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
     }
-  }
 
-  public function logout(Request $request)
-  {
-    $request->user()->currentAccessToken()->delete();
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
 
-    return response()->json([
-      'status' => true
-    ], 200);
-  }
+        return response()->json([
+            'status' => true,
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\API\PongController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CartController;
 use App\Http\Controllers\ProductController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,7 +21,7 @@ Route::post('/auth/register', [AuthController::class, 'register']);
 Route::post('/auth/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
-  Route::post('/auth/logout', [AuthController::class, 'logout']);
+    Route::post('/auth/logout', [AuthController::class, 'logout']);
 });
 
 Route::resource('pongs', PongController::class);
@@ -30,8 +29,8 @@ Route::resource('pongs', PongController::class);
 Route::get('products', ProductController::class);
 
 Route::middleware('auth:sanctum')->prefix('/cart')->group(function () {
-  Route::get('get-cart', [CartController::class, 'getCart']);
-  Route::post('add-item', [CartController::class, 'addItem']);
-  Route::post('remove-item', [CartController::class, 'removeItem']);
-  Route::post('update-quantity', [CartController::class, 'updateQuantity']);
+    Route::get('get-cart', [CartController::class, 'getCart']);
+    Route::post('add-item', [CartController::class, 'addItem']);
+    Route::post('remove-item', [CartController::class, 'removeItem']);
+    Route::post('update-quantity', [CartController::class, 'updateQuantity']);
 });
